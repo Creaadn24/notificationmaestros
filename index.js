b2ce2c59a6e4acf8446af5c228f88a99bc74921a
Creaadn24 <149811499+Creaadn24@users.noreply.github.com>
2025-08-05 16:28:31 -0500

    cambio de index

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function initializeFirebase() {
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
         token_uri: "https://oauth2.googleapis.com/token",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pykxn%40creamaestros-8e80b.iam.gserviceaccount.com",
+        universe_domain: "googleapis.com"
       };
 
       admin.initializeApp({
